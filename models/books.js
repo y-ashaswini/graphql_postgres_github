0b51190f68d5c7e11c86b03f51626b440423f02b
Yashaswini Shivathaya <evilgeniusprevails@gmail.com>
2023-04-04 21:34:38 +0530

    graphql completed

--- models/books.js
+++ models/books.js
@@ -1,25 +1,26 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class books extends Model {
     static associate(models) {
       // define association here
       books.belongsTo(models.authors, {
         foreignKey: "authorId",
-        as : "books",
-        onDelete: "CASCADE"        
-      })
+        as: "books",
+        onDelete: "CASCADE",
+      });
     }
   }
-  books.init({
-    name: DataTypes.STRING,
-    genre: DataTypes.STRING,
-    authorId: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'books',
-  });
+  books.init(
+    {
+      name: DataTypes.STRING,
+      genre: DataTypes.STRING,
+      authorId: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "books",
+    }
+  );
   return books;
 };
--- schema/schema.js
+++ schema/schema.js
@@ -6,8 +6,9 @@ const {
   GraphQLID,
   GraphQLInt,
   GraphQLList,
+  GraphQLNonNull,
 } = graphql;
-// lodash : used for searching inside objects and arrays, instead of having to write the entrie logic by ourselves
+// lodash : used for searching inside objects and arrays, instead of having to write the entire logic by ourselves
 const _ = require("lodash");
 const { books, authors } = require("../models");
 
@@ -50,17 +51,15 @@ const BookType = new GraphQLObjectType({
     genre: {
       type: GraphQLString,
     },
-    author: {
+    author_of_book: {
       type: AuthorType,
       async resolve(parent, args) {
         // return _.find(auth_dummy_data, { id: parent.authorId });
         // console.log(parent.authorId);
-        const getauthors = await authors.findAll({
-          where: {
-            id: parent.authorId,
-          },
+        const getauthor = await authors.findAll({
+          where: { id: parent.authorId },
         });
-        return getauthors;
+        return getauthor[0];
       },
     },
   }),
@@ -79,15 +78,16 @@ const AuthorType = new GraphQLObjectType({
     age: {
       type: GraphQLInt,
     },
-    books: {
-      type: new GraphQLList(BookType),
+    books_by_author: {
+      type: new GraphQLList(BookType), // its a list of books! not an individual book!
       async resolve(parent, args) {
         // return _.filter(books_dummy_data, { authorId: parent.id });
+        console.log("Parent id: ", parent.id);
         const getbooks = await books.findAll({
-          where: {
-            authorId: parent.id,
-          },
+          where: { authorId: parent.id },
         });
+        // console.log("RETURN QUERY:", getbooks);
+        // console.log("-------------------------------------------------");
         return getbooks;
       },
     },
@@ -98,7 +98,7 @@ const AuthorType = new GraphQLObjectType({
 // This is like those routes that you set up, for all books, one particular book, etc.
 
 const RootQuery = new GraphQLObjectType({
-  name: "RootQuery",
+  name: "RootQueryType",
   fields: {
     // this name matters. becasue on the frontend, you use it like
     // book {
@@ -110,48 +110,42 @@ const RootQuery = new GraphQLObjectType({
     book: {
       type: BookType,
       // used on the frontend like
-      // book({id: "123"}) {
+      // book(id: "123") {
       //     name
       //     genre
       // }
       // etc.
       args: {
-        id: {
-          type: GraphQLID,
-        },
+        id: { type: GraphQLID },
+      },
+      async resolve(parent, args) {
+        // return _.find(auth_dummy_data, { id: args.id });
+        const get_book_of_this_id = await books.findAll({
+          where: { id: args.id },
+        });
+        return get_book_of_this_id[0];
+      },
+    },
+
+    author: {
+      type: AuthorType,
+      args: {
+        id: { type: GraphQLID },
       },
       async resolve(parent, args) {
         // code to get data from db/ other source
         // parent : used when we define relations
         // args : the args passed in each query
         // return _.find(books_dummy_data, { id: args.id });
-        const getauthbooks = await books.findAll({
-          where: {
-            id: args.id,
-          },
+        const get_author_of_this_id = await authors.findAll({
+          where: { id: args.id },
         });
-        return getauthbooks;
+        return get_author_of_this_id[0];
       },
     },
-    author: {
-      type: AuthorType,
-      args: {
-        id: {
-          type: GraphQLID,
-        },
-      },
-      async resolve(parent, args) {
-        // return _.find(auth_dummy_data, { id: args.id });
-        const getbookauths = await authors.findAll({
-          where: {
-            id: args.id,
-          },
-        });
-        return getbookauths;
-      },
-    },
+
     // all books
-    books: {
+    booksall: {
       type: new GraphQLList(BookType),
       async resolve(parent, args) {
         // return books_dummy_data;
@@ -160,7 +154,7 @@ const RootQuery = new GraphQLObjectType({
       },
     },
     // all authors
-    authors: {
+    authorsall: {
       type: new GraphQLList(AuthorType),
       async resolve(parent, args) {
         // return auth_dummy_data;
@@ -178,21 +172,23 @@ const Mutation = new GraphQLObjectType({
     addAuthor: {
       type: AuthorType,
       args: {
-        name: { type: GraphQLString },
-        age: { type: GraphQLInt },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
       },
       resolve(parent, args) {
         let author_new = authors.create({ name: args.name, age: args.age });
         return author_new;
       },
     },
+
     addBook: {
       type: BookType,
       args: {
-        name: { type: GraphQLString },
-        genre: { type: GraphQLString },
-        authorId: { type: GraphQLID },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        genre: { type: new GraphQLNonNull(GraphQLString) },
+        authorId: { type: new GraphQLNonNull(GraphQLID) },
       },
+
       resolve(parent, args) {
         let book_new = books.create({
           name: args.name,
